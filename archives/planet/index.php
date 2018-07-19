ac7ee8dfb381e21dfd99693017e516a46746202b
Brian Evans <grknight@gentoo.org>
2018-07-19 08:42:57 -0400

    Update to Tyrian layout

--- archives/planet/index.php
+++ archives/planet/index.php
@@ -1,67 +1,116 @@
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
-<head>
-<title>Planet Gentoo Archives</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link type="image/x-icon" href="/favicon.ico" rel="shortcut icon">
-<link rel="stylesheet" href="/planet.css" type="text/css">
-<link rel="alternate" type="application/rss+xml" title="Planet Gentoo" href="rss20.xml">
-<style type="text/css">
-.row:after,.row:before{display:table;content:" "}
-.row:after{clear:both}
-div.archive-entry { float: left; width: 25%; }
-</style>
+  <head>
+  <meta charset="utf-8">
+  <title>Planet Gentoo Archives</title>
+  <meta name="theme-color" content="#54487a">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta property="og:title" content="Planet Gentoo Archives">
+  <meta property="og:image" content="https://www.gentoo.org/assets/img/logo/gentoo-g.png">
+  <meta name="twitter:image" content="https://www.gentoo.org/assets/img/logo/gentoo-g.png">
+  <link rel="apple-touch-icon" href="https://www.gentoo.org/assets/img/logo/icon-192.png">
+  <link rel="icon" sizes="192x192" href="https://www.gentoo.org/assets/img/logo/icon-192.png">
+  <link href="https://assets.gentoo.org/tyrian/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link href="https://assets.gentoo.org/tyrian/tyrian.min.css" rel="stylesheet" media="screen">
+  <link rel="stylesheet" href="/planet-tyrian.css" type="text/css">
+  <style type="text/css">
+  div.archive-entry { float: left; width: 25%; }
+  </style>
+
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="search" type="application/opensearchdescription+xml" href="https://www.gentoo.org/search/www-gentoo-org.xml" title="Gentoo Website">
+  <link rel="search" type="application/opensearchdescription+xml" href="https://www.gentoo.org/search/forums-gentoo-org.xml" title="Gentoo Forums">
+  <link rel="search" type="application/opensearchdescription+xml" href="https://www.gentoo.org/search/bugs-gentoo-org.xml" title="Gentoo Bugzilla">
+  <link rel="search" type="application/opensearchdescription+xml" href="https://www.gentoo.org/search/packages-gentoo-org.xml" title="Gentoo Packages">
+  <link rel="search" type="application/opensearchdescription+xml" href="https://www.gentoo.org/search/archives-gentoo-org.xml" title="Gentoo List Archives">
+  <link rel="alternate" type="application/rss+xml" title="Planet Gentoo" href="rss20.xml">
+  <link rel="alternate" type="application/atom+xml" title="Planet Gentoo" href="atom.xml">
 </head>
 
-<body>
-<table cellpadding="0" cellspacing="0" width="100%" border="0">
-<tbody><tr>
+  <body class="">
+    <header>
+  <div class="site-title">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="site-title-buttons">
+          <div class="btn-group btn-group-sm">
+            <a href="https://get.gentoo.org/" role="button" class="btn get-gentoo"><span class="fa fa-fw fa-download"></span> <strong>Get Gentoo!</strong></a>
+            <div class="btn-group btn-group-sm">
+              <a class="btn gentoo-org-sites dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
+                <span class="fa fa-fw fa-map-o"></span> <span class="hidden-xs">gentoo.org sites</span> <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu dropdown-menu-right">
+                <li><a href="https://www.gentoo.org/" title="Main Gentoo website"><span class="fa fa-home fa-fw"></span> gentoo.org</a></li>
+                <li><a href="https://wiki.gentoo.org/" title="Find and contribute documentation"><span class="fa fa-file-text-o fa-fw"></span> Wiki</a></li>
+                <li><a href="https://bugs.gentoo.org/" title="Report issues and find common issues"><span class="fa fa-bug fa-fw"></span> Bugs</a></li>
+                <li><a href="https://forums.gentoo.org/" title="Discuss with the community"><span class="fa fa-comments-o fa-fw"></span> Forums</a></li>
+                <li><a href="https://packages.gentoo.org/" title="Find software for your Gentoo"><span class="fa fa-hdd-o fa-fw"></span> Packages</a></li>
+                <li class="divider"></li>
+                <li><a href="https://planet.gentoo.org/" title="Find out what's going on in the developer community"><span class="fa fa-rss fa-fw"></span> Planet</a></li>
+                <li><a href="https://archives.gentoo.org/" title="Read up on past discussions"><span class="fa fa-archive fa-fw"></span> Archives</a></li>
+                <li><a href="https://gitweb.gentoo.org/" title="Browse our source code in Gitweb"><span class="fa fa-code fa-fw"></span> Gitweb</a></li>
+                <li><a href="https://sources.gentoo.org/" title="Browse our source code in CVS"><span class="fa fa-code fa-fw"></span> CVS Sources</a></li>
+                <li class="divider"></li>
+                <li><a href="https://infra-status.gentoo.org/" title="Get updates on the services provided by Gentoo"><span class="fa fa-server fa-fw"></span> Infra Status</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="logo">
+          <a href="/" title="Back to the homepage" class="site-logo">
+            <object data="https://assets.gentoo.org/tyrian/site-logo.svg" type="image/svg+xml">
+            <img src="https://assets.gentoo.org/tyrian/site-logo.png" alt="Gentoo Linux Logo">
+            </object>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav class="tyrian-navbar" role="navigation">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="collapse navbar-collapse navbar-main-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/">Home</a></li>
+            <li class=""><a href="/archives/">Archives</a></li>
+            <li class=""><a href="/universe/">Gentoo Universe</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class=""><a href="https://www.gentoo.org/donate/"><span class="fa fa-heart"></span> Donate</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
 
 
-<td class="logobox">
-<a href="https://www.gentoo.org/"><img alt="Gentoo Logo" src="/images/gtop-s.jpg" width="193" height="88"></a><br>
-<a href="https://www.gentoo.org/"><img alt="Gentoo Logo Side" src="/images/gbot-s.gif" width="193" height="31"></a>
-</td>
-
-
-<td class="linkbar">
-<div class="menu">
-<a href="https://www.gentoo.org/get-started/about/" class="menulink">About</a> |
-          <a href="https://wiki.gentoo.org/wiki/Project:Gentoo" class="menulink">Projects</a> |
-          <a href="https://www.gentoo.org/support/documentation" class="menulink">Docs</a> |
-          <a href="https://forums.gentoo.org/" class="menulink">Forums</a> |
-          <a href="https://www.gentoo.org/get-involved/mailing-lists/" class="menulink">Lists</a> |
-          <a href="https://bugs.gentoo.org/" class="menulink">Bugs</a> |
-          <a href="https://www.gentoo.org/downloads/" class="menulink">Get Gentoo!</a> |
-          <a href="https://www.gentoo.org/support/" class="menulink">Support</a> |
-          <a href="https://wiki.gentoo.org/wiki/Main_Page" class="menulink">Wiki</a> |
-          <a href="https://planet.gentoo.org/" class="menulink">Planet</a>
-</div></td>
-
-
-</tr>
-<tr>
-
-
-<td class="leftmenu">
-<img alt="Gentoo Spaceship" src="/images/gridtest.gif" align="right" height="96" width="132"><br clear="all"><br>
-
-<div class='altmenu'>
+    <div class="container-fluid">
+      <div class="row">
+        <div id="left-column" class="col-md-3">
+<div class="altmenu">
 <br>Bugs? Comments? Suggestions? <a href="https://wiki.gentoo.org/wiki/Project:Planet">Contact us!</a>
 
 <br><br><b>Powered by:</b><br>
 <a href="http://www.intertwingly.net/code/venus/">Planet Venus</a>
 
 </div>
-</td>
-
-
-<td class="content">
-
-<div id="intro">
-<img src="/images/gentoo.gif"><p>Welcome to the <b>Planet Gentoo</b> archives.  Also available for <a href="http://planet.gentoo.org/universe/archives/">Gentoo Universe</a>.</p>
 </div>
+
+<div id="content" class="col-md-9">
+<div class="media">
+<div class="media-left"><img src="/images/gentoo.gif" class="media-object"></div>
+<div class="media-body">Welcome to the <b>Planet Gentoo</b> archives.  Also available for <a href="http://planet.gentoo.org/universe/archives/">Gentoo Universe</a>.</div>
+</div><br />
 
 <?php
 	class SortedFileIterator extends SplHeap {
@@ -88,11 +137,37 @@ div.archive-entry { float: left; width: 25%; }
 	}
 ?>
 
-</td></tr>
-
-
-<tr><td colspan="2" class="footer">
-Gentoo Design, Copyright 2001-2018 Gentoo Foundation, Inc.<br>
-Views expressed in the content shown above do not necessarily represent the views of Gentoo Linux or the Gentoo Foundation.
-</td></tr>
-</tbody></table></body></html>
+</div>
+</div>
+</div>
+<footer>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-3 col-md-2">
+        <ul class="footerlinks three-icons">
+          <li><a href="https://twitter.com/gentoo" title="@Gentoo on Twitter"><span class="fa fa-twitter fa-fw"></span></a></li>
+          <li><a href="https://plus.google.com/+Gentoo" title="+Gentoo on Google+"><span class="fa fa-google-plus fa-fw"></span></a></li>
+          <li><a href="https://www.facebook.com/gentoo.org" title="Gentoo on Facebook"><span class="fa fa-facebook fa-fw"></span></a></li>
+        </ul>
+      <div>
+        <div class="sitemap text-center">
+          <a href="https://wiki.gentoo.org/wiki/Foundation:Privacy_Policy">Privacy Policy</a>
+        </div>
+      </div>
+      </div>
+      <div class="col-xs-9 col-md-9">
+        <strong>&copy; 2001&ndash;2018 Gentoo Foundation, Inc.</strong><br>
+        <small>
+          Gentoo is a trademark of the Gentoo Foundation, Inc.
+          The contents of this document, unless otherwise expressly stated, are licensed under the
+          <a href="https://creativecommons.org/licenses/by-sa/3.0/" rel="license">CC-BY-SA-3.0</a> license.
+          The <a href="https://www.gentoo.org/inside-gentoo/foundation/name-logo-guidelines.html">Gentoo Name and Logo Usage Guidelines</a> apply.
+        </small>
+      </div>
+    </div>
+  </div>
+</footer>
+    <script src="https://assets.gentoo.org/tyrian/jquery.min.js"></script>
+    <script src="https://assets.gentoo.org/tyrian/bootstrap.min.js"></script>
+  </body>
+</html>
