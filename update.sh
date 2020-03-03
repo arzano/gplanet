971ff33e6cfa9e866493fd087098955ddcf0b14b
Max Magorsch <arzano@gentoo.org>
2020-03-03 02:49:33 +0100

    Create the output directories if they are not present
    Signed-off-by: Max Magorsch <arzano@gentoo.org>

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 USER=${1:gplanet}
 GROUP=${2:gplanet}
 
+mkdir -p htdocs data
+
 docker build --build-arg USER_ID=$(id -u $USER) \
              --build-arg GROUP_ID=$(id -g $GROUP)  \
              -t gentoo/planet-pluto:latest .
